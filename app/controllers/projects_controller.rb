af88de6d7570b319944792631fec93319ea3d548
Daria Approvato <dariaapprovato@gmail.com>
2023-05-18 01:01:09 -0400

    added project controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,44 @@
+class ProjectsController < ApplicationController
+
+    def index
+        projects = Project.all
+        render json: projects
+    end
+
+    def show
+        project = Project.find_by(id: params[:id])
+        render json: project
+    end
+
+    def create
+        project = Project.create(project_params)
+        render json: project, status: :created
+    end
+
+    def update
+        project = Project.find_by(id: params[:id])
+        if project
+            project.update(project_params)
+            render json: project
+        else
+            render json: { error: "Project not found" }, status: :not_found
+        end
+    end
+
+    def destroy
+        project = Project.find_by(id: params[:id])
+        if project
+            project.destroy
+            head :no_content
+        else
+            render json: { error: "Project not found" }, status: :not_found
+        end
+    end
+
+    private
+
+    def project_params
+        params.permit(:title, :description)
+    end
+
+end
