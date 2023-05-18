3794e417feb429267ba5afaf040fd3825f34980f
Daria Approvato <dariaapprovato@gmail.com>
2023-05-18 01:05:17 -0400

    updated models routes schema and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_16_222110) do
+ActiveRecord::Schema.define(version: 2023_05_17_211239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2023_05_16_222110) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "project_id"
   end
 
   create_table "users", force: :cascade do |t|
