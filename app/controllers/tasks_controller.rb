79cb101e22482cb650a0298ebfca9eb6cd3f5077
Daria Approvato <dariaapprovato@gmail.com>
2023-05-18 01:01:47 -0400

    added task controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+
+    def index
+        tasks = Task.all
+        render json: tasks
+    end
+
+    def show
+        task = Task.find_by(id: params[:id])
+        render json: task
+    end
+
+    def create
+        task = Task.create(content: params[:content])
+        render json: task, status: :created
+    end
+
+    def update
+        task = Task.find_by(id: params[:id])
+        if task
+            task.update(task_params)
+            render json: task
+        else
+            render json: { error: "Task not found" }, status: :not_found
+        end
+    end
+
+    def destroy
+        task = Task.find_by(id: params[:id])
+        if task
+            task.destroy
+            head :no_content
+        else
+            render json: { error: "Task not found" }, status: :not_found
+        end
+    end
+
+    private
+
+    def task_params
+        params.permit(:content)
+    end
+
+end
